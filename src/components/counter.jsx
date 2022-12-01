b538a87abdc3bb218188276de105b84b059aba02
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-01 23:21:16 +0530

    Embedding expressions done

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
  class Counter extends Component {
     
     render() { 
-        return <h1>Hello World</h1>;//jsx expression
+        return <React.Fragment><h1>Hello World</h1>
+        <button>Increament</button></React.Fragment> ;//jsx expression
     }
 }
  
