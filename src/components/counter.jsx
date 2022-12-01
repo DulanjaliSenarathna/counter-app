6881400a6f907c464705683990681071cca4562d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-01 23:07:31 +0530

    First component

---
+++ src/components/counter.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+ class Counter extends Component {
+    
+    render() { 
+        return <h1>Hello World</h1>;//jsx expression
+    }
+}
+ 
+export default Counter;//can be add export default to begining of the class counter
