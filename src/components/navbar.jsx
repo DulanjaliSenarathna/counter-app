5ebc56ea4115344d4478e29c7f6c2d2323a84d6d
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-06 12:51:40 +0530

    Stateless functional components

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
-    
-    render() { 
-        return (<nav class="navbar bg-light">
+//there is no 'state' in this class component. then we can use Stateless Functional Component. shortcut => sfc
+
+const NavBar = (props) => { //like class component, we don't need this.props.totalCounters. just add props as a parameter
+   
+    return (<nav class="navbar bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar
-          <span className='badge badge-pill badge-secondary'>{this.props.totalCounters}</span>
+          <span className='badge badge-pill badge-secondary'>{props.totalCounters}</span>
           </a>
         </div>
       </nav>);
-    }
+  
 }
  
 export default NavBar;
