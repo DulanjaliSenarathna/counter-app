c4b22c41833d1020ebc9297a84d0948ac71fdf5f
Dulanjali Senarathna <dishanisenarathna96@gmail.com>
2022-12-06 13:48:00 +0530

    Destructuring arguments

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,21 +1,21 @@
 import React, { Component } from 'react'
 import Counter from './counter';
 
-class Counters extends Component {
-    
+class Counters extends Component {  
 
     render() { 
+        const {onReset, counters, onDelete, onIncrement} = this.props;//destructuring
         return (
 
         <div>
-            <button onClick={this.props.onReset} className='btn btn-primary btn-sm m-2'>
+            <button onClick={onReset} className='btn btn-primary btn-sm m-2'>
                 Reset
             </button>
            {this.props.counters.map(counter => (
            <Counter 
            key={counter.id} 
-           onDelete={this.props.onDelete}
-           onIncrement ={this.props.onIncrement}
+           onDelete={onDelete}
+           onIncrement ={onIncrement}
           // value={counter.value}  instead of adding separate props, it's easy to add counter object. then we can access all prperties inside it
           // selected={counter.selected} 
           counter={counter}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 
 //there is no 'state' in this class component. then we can use Stateless Functional Component. shortcut => sfc
 
-const NavBar = (props) => { //like class component, we don't need this.props.totalCounters. just add props as a parameter
+const NavBar = ({totalCounters}) => { //like class component, we don't need this.props.totalCounters. just add props as a parameter, but usng object destructuring, we just need to add {totalCounters} as parameter
    
     return (<nav class="navbar bg-light">
         <div className="container-fluid">
           <a className="navbar-brand" href="#">Navbar
-          <span className='badge badge-pill badge-secondary'>{props.totalCounters}</span>
+          <span className='badge badge-pill badge-secondary'>{totalCounters}</span>
           </a>
         </div>
-      </nav>);
+      </nav>);//instead of props.totalCounters , we can use {totalCounters} using object destructuring
   
 }
  
